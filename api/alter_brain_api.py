2ae5aa756c30d1a44a62633cb6bea31a6b3730f3
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2025-08-10 14:48:52 +0200

    update get context

--- api/alter_brain_api.py
+++ api/alter_brain_api.py
@@ -90,7 +90,7 @@ def handle_req():
     if index.startswith("http://") or index.startswith("https://"):
         url = index
         index_filename= "datas/" + lib__embedded_context.build_index_url(url) + "/emb_index.csv"
-    context = lib__embedded_context.find_context(text, index_filename, n_results=3)
+    context = lib__embedded_context.find_context(text, index_filename, n_results=10)
     res = [{'id':1,'request':'searchcontext','answer':context}]
     response = jsonify(res)
     response.headers['Content-Type'] = 'application/json'
