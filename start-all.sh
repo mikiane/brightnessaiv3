d0cac3f491ee5ee768bf531ce4b711776311cac8
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2025-08-10 08:39:05 +0200

    update create virtual env

--- start-all.sh
+++ start-all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Définir le chemin de l'environnement virtuel
+VENV_PATH="/home/michel/myenv"
+GUNICORN="${VENV_PATH}/bin/gunicorn"
+
 # Lancement de Gunicorn avec la configuration spécifique
-/home/michel/brightnessaiv3/venv/bin/gunicorn --bind 0.0.0.0:8000 -w 4 api.streaming_api:app --timeout 1200 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
+${GUNICORN} --bind 0.0.0.0:8000 -w 8 api.streaming_api:app --timeout 14400 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
 
-/home/michel/brightnessaiv3/venv/bin/gunicorn --bind 0.0.0.0:8001 -w 4 api.transformers_api:app --timeout 7200 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
+${GUNICORN} --bind 0.0.0.0:8001 -w 8 api.transformers_api:app --timeout 14400 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
 
-/home/michel/brightnessaiv3/venv/bin/gunicorn --bind 0.0.0.0:8002 -w 4 api.alter_brain_api:app --timeout 1200 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
+${GUNICORN} --bind 0.0.0.0:8002 -w 8 api.alter_brain_api:app --timeout 14400 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
